d6dddd41d1af918c28cae78262cdee6f33268f7c
Midokhalil1 <midokhalil22@gmail.com>
2022-10-27 16:16:22 -0400

    Movies

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer()  {
+    return (
+        <footer>
+            <p>&copy; 2022 Mido Khalil.
+               Source: <a href="https://sampleapis.com/api-list/movies">
+                sampleapis.com</a> </p>
+               
+"
+        </footer>
+    )
+
+} 
+export default Footer
